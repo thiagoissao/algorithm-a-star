2c1c0ac8ce6af8449cdba17cd4a8705772779b86
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2021-03-23 20:13:34 -0300

    Implement first heuristic

--- main.cpp
+++ main.cpp
@@ -1,33 +1,57 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <string.h>
+#include <sstream>
 
 using namespace std;
+std::stringstream ss;
 
 vector<int> EXPECTED_FINAL_STATE = {1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 0};
 
-// vector<string> split(const string &s, char delimiter)
-// {
-//   vector<string> tokens;
-//   string token;
-//   istringstream tokenStream(s);
-//   while (getline(tokenStream, token, delimiter))
-//   {
-//     tokens.push_back(token);
-//   }
-//   return tokens;
-// }
-
-void convertStringToArray(string str)
+void log(vector<int> vec)
 {
-  // vector<string> state = split(str, ' ');
-  // cout << EXPECTED_FINAL_STATE;
+  for (int i = 0; i < vec.size(); i++)
+  {
+    cout << vec[i] << " ";
+  }
+}
+
+vector<int> split(const string &str, char delim = ' ')
+{
+  stringstream ss(str);
+  string tok;
+  vector<int> vec;
+  while (getline(ss, tok, delim))
+  {
+    if (!tok.empty())
+      vec.push_back(stoi(tok));
+  }
+  return vec;
+}
+
+int h1(vector<int> finalState, vector<int> currentState)
+{
+  int sumOfPartsOutPlace = 0;
+  for (int i = 0; i < finalState.size(); i++)
+  {
+    if (finalState[i] != 0 && finalState[i] != currentState[i])
+    {
+      sumOfPartsOutPlace += 1;
+    }
+  }
+  return sumOfPartsOutPlace;
 }
 
 int main()
 {
-  string initialState = "a";
-  // cin >> initialState;
-  cout << 'a';
+  string initialState;
+  getline(cin, initialState);
+  vector<int> currentState = split(initialState);
+
+  int sumH1 = h1(EXPECTED_FINAL_STATE, currentState);
+
+  cout << sumH1 << endl;
+
   return 0;
 }
---
+++ tests-cases-step-three/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 23/03/2021 09:52:35 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Fase 3 - Teste - Tabuleiro
