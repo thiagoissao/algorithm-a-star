bc793d489e7457f642e718d180750f71da4a1a00
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2021-03-23 07:43:48 -0300

    Inicia terceira etapa do trabalho

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+vector<int> EXPECTED_FINAL_STATE = {1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 0};
+
+// vector<string> split(const string &s, char delimiter)
+// {
+//   vector<string> tokens;
+//   string token;
+//   istringstream tokenStream(s);
+//   while (getline(tokenStream, token, delimiter))
+//   {
+//     tokens.push_back(token);
+//   }
+//   return tokens;
+// }
+
+void convertStringToArray(string str)
+{
+  // vector<string> state = split(str, ' ');
+  // cout << EXPECTED_FINAL_STATE;
+}
+
+int main()
+{
+  string initialState = "a";
+  // cin >> initialState;
+  cout << 'a';
+  return 0;
+}
