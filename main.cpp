a1d4121b6ede5b0c5e40ab37991101c3a2c94084
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2021-03-23 20:18:13 -0300

    convert int to unsigned int

--- main.cpp
+++ main.cpp
@@ -7,21 +7,21 @@
 using namespace std;
 std::stringstream ss;
 
-vector<int> EXPECTED_FINAL_STATE = {1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 0};
+vector<unsigned int> EXPECTED_FINAL_STATE = {1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 0};
 
-void log(vector<int> vec)
+void log(vector<unsigned int> vec)
 {
-  for (int i = 0; i < vec.size(); i++)
+  for (unsigned int i = 0; i < vec.size(); i++)
   {
     cout << vec[i] << " ";
   }
 }
 
-vector<int> split(const string &str, char delim = ' ')
+vector<unsigned int> split(const string &str, char delim = ' ')
 {
   stringstream ss(str);
   string tok;
-  vector<int> vec;
+  vector<unsigned int> vec;
   while (getline(ss, tok, delim))
   {
     if (!tok.empty())
@@ -30,10 +30,10 @@ vector<int> split(const string &str, char delim = ' ')
   return vec;
 }
 
-int h1(vector<int> finalState, vector<int> currentState)
+unsigned int h1(vector<unsigned int> finalState, vector<unsigned int> currentState)
 {
-  int sumOfPartsOutPlace = 0;
-  for (int i = 0; i < finalState.size(); i++)
+  unsigned int sumOfPartsOutPlace = 0;
+  for (unsigned int i = 0; i < finalState.size(); i++)
   {
     if (finalState[i] != 0 && finalState[i] != currentState[i])
     {
@@ -47,9 +47,9 @@ int main()
 {
   string initialState;
   getline(cin, initialState);
-  vector<int> currentState = split(initialState);
+  vector<unsigned int> currentState = split(initialState);
 
-  int sumH1 = h1(EXPECTED_FINAL_STATE, currentState);
+  unsigned int sumH1 = h1(EXPECTED_FINAL_STATE, currentState);
 
   cout << sumH1 << endl;
 
